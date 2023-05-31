a4f16a38589745fa7fc2b94cc467b0ac3e2f2354
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-05-31 14:16:48 -0700

    Create User schema and model that stores the users sessions

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,6 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
-
-// import schema from Question.js
-const questionSchema = require('./Question');
+const { Schema, model, Types } = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const userSchema = new Schema(
   {
@@ -10,31 +8,52 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
+      validate: {
+        // ensures that the username is not empty
+        validator: (value) => value.trim().length > 0,
+        message: "Username cannot be empty",
+      },
     },
     email: {
       type: String,
       required: true,
       unique: true,
-      match: [/.+@.+\..+/, 'Must use a valid email address'],
+      validate: {
+        // ensures the inputted email is valid
+        validator: validator.isEmail,
+        message: "Must use a valid email address",
+      },
     },
     password: {
       type: String,
       required: true,
+      validate: {
+        validator: function (value) {
+          // ensures the passord is more than 8 characters and contains a special character
+          return value.length >= 8 && /[!@#$%^&*(),.?":{}|<>]/.test(value);
+        },
+        message:
+          "Password must be at least 8 characters long and contain a special character",
+      },
     },
-    // set savedQuestions to be an array of data that adheres to the questionSchema
-    savedQuestions: [questionSchema],
+    // set the users saved sessions to be an array of data that adheres to the sessionSchema
+    sessions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Session",
+      },
+    ],
   },
-  // set this to use virtual below
   {
-    toJSON: {
-      virtuals: true,
-    },
+    timestamps: true,
   }
 );
 
-// hash user password
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
+// ---------- MIDDLEWARE ----------
+
+// encrypts/hashes the user's inputted password using the bcrypt library
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
@@ -42,16 +61,13 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+// ---------- SCHEMA METHODS ----------
+
 // custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-// when we query a user, we'll also get another field called `questionCount` with the number of saved questions we have
-userSchema.virtual('questionCount').get(function () {
-  return this.savedQuestions.length;
-});
-
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
