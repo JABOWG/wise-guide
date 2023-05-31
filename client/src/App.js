6a910d7d9f6ba6a3d15d4ae8b18fd53c50e943e3
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-05-31 15:01:22 -0700

    Commented out client files since it was causing errors due to files not being created yet

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 // import your components
-import NewQuestion from "./components/NewQuestion";
-import QuestionHistory from "./components/QuestionHistory";
+// import NewQuestion from "./components/NewQuestion";
+// import QuestionHistory from "./components/QuestionHistory";
 import Navbar from "./components/Navbar";
 
 // The ApolloProvider component wraps the entire application and allows us to share data between all of the components that we will create
@@ -28,13 +23,13 @@ function App() {
       <Router>
         <>
           <Navbar />
-          <Switch>
+          <Routes>
             {/* new routes for NewQuestion and QuestionHistory */}
-            <Route exact path="/new-question" component={NewQuestion} />
-            <Route exact path="/question-history" component={QuestionHistory} />
+            {/* <Route exact path="/new-question" component={NewQuestion} /> */}
+            {/* <Route exact path="/question-history" component={QuestionHistory} /> */}
             {/* redirect to '/new-question' if no match */}
-            <Route render={() => <Redirect to="/new-question" />} />
-          </Switch>
+            {/* <Route render={() => <Redirect to="/new-question" />} /> */}
+          </Routes>
         </>
       </Router>
     </ApolloProvider>
