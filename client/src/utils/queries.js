73c55fc407840bda5df383da384a748b559f7996
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-06-03 19:34:42 -0700

    Updated client-side mutations and queries to reflect type definition changes on server-side

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -18,9 +18,22 @@ export const GET_ME = gql`
   }
 `;
 
+export const GET_RECENT_SESSIONS = gql`
+  query getRecentSession($limit: Int!) {
+    recentSessions(limit: $limit) {
+      _id
+      messages {
+        _id
+        aiResponse
+        userQuestion
+      }
+    }
+  }
+`;
+
 export const GET_ALL_SESSIONS = gql`
-  query GetAllSessions {
-    getAllSessions {
+  query getAllSessions {
+    allSessions {
       _id
       messages {
         _id
@@ -32,8 +45,8 @@ export const GET_ALL_SESSIONS = gql`
 `;
 
 export const GET_SESSION = gql`
-  query getSession($sessionId: ID!) {
-    getSession(sessionId: $sessionId) {
+  query getSingleSession($sessionId: ID!) {
+    session(sessionId: $sessionId) {
       _id
       messages {
         _id
