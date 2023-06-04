f02cfc75342196d9042770a9fa9742cbbe41a8b7
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-06-03 20:32:28 -0700

    Added authentication handling using JSON Web Token (jwt)

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Authservice from "../../utils/auth"
+import Authservice from "../../utils/auth";
 
 const Navbar = () => {
   return (
+    // Render the navigation bar
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
+        {/* Link to the home page */}
         <Link to="/" className="navbar-item">
           Home
         </Link>
       </div>
       <div className="navbar-menu">
         <div className="navbar-end">
+          {/* Link to the sessions page */}
           <Link to="/sessions" className="navbar-item">
             My Sessions
           </Link>
+          {/* Link to the user's profile page */}
           <Link to="/me" className="navbar-item">
             My Profile
           </Link>
+          {/* Link to the user login/signup form */}
           <Link to="/userform" className="navbar-item">
             Signup/Login
           </Link>
+          {/* Logout button */}
           <button
             className="btn btn-lg btn-light m-2"
             onClick={Authservice.logout}
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,38 +1,44 @@
 import decode from "jwt-decode";
 
 class AuthService {
-  getProfile() {
+  // Get the decoded user object from the token
+  getUser() {
     return decode(this.getToken());
   }
 
+  // Check if the user is logged in
   loggedIn() {
     const token = this.getToken();
-    return !!token && !this.isTokenExpired(token);
+    return token && !this.isTokenExpired(token) ? true : false;
   }
 
+  // Check if the token is expired
   isTokenExpired(token) {
-    try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else return false;
-    } catch (err) {
-      return false;
+    const decoded = decode(token);
+    // Compare the token's expiration time with the current time
+    if (decoded.exp < Date.now() / 1000) {
+      // If the token is expired, remove it from localStorage and return true
+      localStorage.removeItem("id_token");
+      return true;
     }
+    return false;
   }
 
+  // Get the token from localStorage
   getToken() {
     return localStorage.getItem("id_token");
   }
 
+  // Perform login by storing the token in localStorage and redirecting to the home page
   login(idToken) {
     localStorage.setItem("id_token", idToken);
     window.location.assign("/");
   }
 
+  // Perform logout by removing the token from localStorage and redirecting to the login/signup page
   logout() {
     localStorage.removeItem("id_token");
-    window.location.assign("/");
+    window.location.assign("/userform");
   }
 }
 
