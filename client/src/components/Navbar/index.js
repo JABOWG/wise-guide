b3096befcf72e2e7b6ea6b260d3f99305656513d
huyingg1 <yinggebarry@gmail.com>
2023-06-06 15:54:00 -0400

    change icons and add donate button

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -7,11 +7,28 @@ const Navbar = () => {
     <>
       {/* Render the navigation bar */}
       {Authservice.loggedIn() ? (
-        <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+        <nav
+          className="navbar is-dark"
+          role="navigation"
+          aria-label="main navigation"
+        >
           <div className="navbar-brand">
             {/* Link to the home page */}
-            <Link to="/" className="navbar-item is-size-4">
-              Wise Guide
+            <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+              <h1 className="title is-1" style={{ color: "white" }}>
+                Wise Guide!
+              </h1>
+
+              <img
+                src={`${process.env.PUBLIC_URL}/Project-logo.png`}
+                alt="Logo"
+                style={{
+                  marginLeft: "8px",
+                  marginBottom: "8px",
+                  width: "50px",
+                  height: "50px",
+                }}
+              />
             </Link>
           </div>
           <div className="navbar-menu">
@@ -24,6 +41,13 @@ const Navbar = () => {
               <Link to="/me" className="navbar-item is-size-4">
                 My Profile
               </Link>
+              {/* Link to the user's profile page */}
+              <a
+                className="navbar-item is-size-4"
+                href={process.env.REACT_APP_STRIPE_CHECKOUT_URL}
+              >
+                Buy Me a Coffee
+              </a>
               {/* Logout button */}
               <Link
                 className="navbar-item is-size-4"
