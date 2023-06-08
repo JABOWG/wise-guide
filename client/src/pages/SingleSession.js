a620dccacf70f69779a435cfe729f3b6cd6d5012
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-06-07 19:23:58 -0700

    redeploying

--- client/src/pages/SingleSession.js
+++ client/src/pages/SingleSession.js
@@ -109,7 +109,16 @@ const SingleSession = () => {
   const renderChatBubbles = () => {
     // Check if there are no messages in the session data
     if (!sessionData.messages || sessionData.messages.length === 0) {
-      return <div>Ask Away!</div>; // Render a message indicating no messages
+      if (messageLoading) {
+        return (
+          <div className="message is-danger">
+            <div className="message-body">
+              Error occurred while creating message
+            </div>
+          </div>
+        );
+      }
+      else { return <div>Ask Away!</div>; } // Render a message indicating no messages
     }
 
     // Map through the messages in the session data
