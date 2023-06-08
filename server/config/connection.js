9c0408dfeae7b568af05c75c15f6799728adf6d6
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-06-07 19:16:53 -0700

    redeploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,4 +3,3 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tech-friends');
 
 module.exports = mongoose.connection;
-// try to rebuild
